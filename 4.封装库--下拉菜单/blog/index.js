f5c4b25f5dea037b33cdc374c44e0c5a7962e8ec
Aldrich <aldrich.fork@live.com>
2017-08-13 10:13:19 +0800

    封装库-下拉菜单

--- 4.封装库--下拉菜单/blog/index.js
+++ 4.封装库--下拉菜单/blog/index.js
@@ -2,10 +2,10 @@
 
 window.onload = function () {
 	$().getClass('member').hover(function () {
-        $(this).css('background', 'url(images/arrow2.png) no-repeat 55px center');
+        $(this).css('background', 'url(images/arrow2.png) no-repeat 65px center');
 		$().getTagName('ul').show();
     },function () {
-        $(this).css('background', 'url(images/arrow.png) no-repeat 55px center');
+        $(this).css('background', 'url(images/arrow.png) no-repeat 65px center');
         $().getTagName('ul').hide();
     })
 };
